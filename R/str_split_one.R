2303fa2a9ed3cb33b8197a0755810e1d9964638c
Adrian Cantu <garbanyo@gmail.com>
2022-03-04 19:52:11 +0000

    now working

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -1,16 +1,5 @@
 #' Split a string
 #'
-#' @param x A character vector with one element.
-#' @param split What to split on.
-#'
-#' @return A character vector.
-#' @export
-#'
-#' @examples
-#' x <- "alfa,bravo,charlie,delta"
-#' strsplit1(x, split = ",")
-#' Split a string
-#'
 #' @param string A character vector with, at most, one element.
 #' @inheritParams stringr::str_split
 #'
