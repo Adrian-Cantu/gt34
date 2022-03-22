5d2f511af48f6f3e651773376ed94736cb96e371
Adrian Cantu <garbanyo@gmail.com>
2022-03-22 18:15:57 +0000

    ready to test template

--- R/trial_report.R
+++ R/trial_report.R
@@ -1,6 +1,13 @@
-rmarkdown::render(system.file('report_template','trial_report.Rmd',package='gt34'),
-                  output_file = 'tttest',
-                  output_dir = '.',
-                  #intermediates_dir='.',
-                  params = list('rds_file'=file.path(getwd(),'Gill_intSites.rds'))
-                 )
+#' render trial report
+#'
+#' @param df_file the path to a dataframe of intsites
+#'
+#' @export
+trial_report <- function(df_file){
+
+  rmarkdown::render(system.file('report_template','trial_report.Rmd',package='gt34'),
+    output_file = 'tttest',
+    output_dir = '.',
+    params = list('rds_file'=file.path(getwd(),df_file))
+  )
+}
